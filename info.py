504261797554d8ab07de87d98e41d89c76d28d89
Tahimpro <133028260+Tahimpro@users.noreply.github.com>
2023-05-19 17:38:24 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,7 +13,7 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ['API_ID','25804684'])
+API_ID = int(environ['API_ID',"25804684"])
 API_HASH = environ['API_HASH']
 BOT_TOKEN = environ['BOT_TOKEN']
 
